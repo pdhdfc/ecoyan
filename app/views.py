ec8ed0c71939962ce169e8714fce03f092b77afe
DeveloperTKI <109338354+DeveloperTKI@users.noreply.github.com>
2024-05-22 19:14:01 +0530

    home

--- app/views.py
+++ app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your views here.
 def index(request):
@@ -74,3 +76,6 @@ from django.shortcuts import render
 
 def custom_404_view(request, exception):
     return render(request, '404.html', status=404)
+
+
+
